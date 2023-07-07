1a7e11a92e3d8ee91192c81c098c5729954e3927
danipetro21 <idany346@gmail.com>
2023-07-07 12:47:27 +0200

    add create CRUD

--- src/main/java/com/java/daniele/fotoalbum/controller/FotoController.java
+++ src/main/java/com/java/daniele/fotoalbum/controller/FotoController.java
@@ -4,10 +4,12 @@ package com.java.daniele.fotoalbum.controller;
 import com.java.daniele.fotoalbum.model.Foto;
 import com.java.daniele.fotoalbum.repository.CategoriaRepository;
 import com.java.daniele.fotoalbum.repository.FotoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -26,7 +28,7 @@ public class FotoController {
     private CategoriaRepository categoriaRepository;
 
     @GetMapping("/lista")
-    public String listFoto(@RequestParam(name = "keyword", required = false) String searchString, Model model){
+    public String listFoto(@RequestParam(name = "keyword", required = false) String searchString, Model model) {
         List<Foto> listaFoto;
 
         if (searchString == null || searchString.isBlank()) {
@@ -35,13 +37,13 @@ public class FotoController {
             listaFoto = fotoRepository.findByTitleContainingIgnoreCase(searchString);
         }
 
-        model.addAttribute("listaFoto" , listaFoto);
+        model.addAttribute("listaFoto", listaFoto);
         model.addAttribute("searchInput", searchString == null ? "" : searchString);
         return "/foto/gallery";
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(@PathVariable("id") Integer id, Model model){
+    public String detail(@PathVariable("id") Integer id, Model model) {
 
         Optional<Foto> result = fotoRepository.findById(id);
         if (result.isPresent()) {
@@ -54,26 +56,41 @@ public class FotoController {
     }
 
     @PostMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id){
+    public String delete(@PathVariable Integer id) {
 
         Foto fotoToDelete = getFotoById(id);
         fotoRepository.delete(fotoToDelete);
         return "redirect:/foto/lista";
     }
 
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("foto", new Foto());
+        model.addAttribute("categorie", categoriaRepository.findAll());
+        return "/foto/form";
+    }
 
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("foto") Foto fotoForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("foto", fotoForm);
+            model.addAttribute("categorie", categoriaRepository.findAll());
+            return "/foto/form";
+        }
 
+        fotoRepository.save(fotoForm);
 
+        return "redirect:/foto/lista";
+    }
 
     /*UTILITY*/
 
-    public Foto getFotoById(Integer id){
+    public Foto getFotoById(Integer id) {
         Optional<Foto> result = fotoRepository.findById(id);
         if (result.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "foto with id " + id + " not found");
         }
         return result.get();
     }
-
 
 }
